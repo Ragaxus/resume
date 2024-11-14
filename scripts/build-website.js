01b75ef787a70ed5f8d01f4457d11965ab8e1f3f
Stav Gold <ragaxus@gmail.com>
2024-11-14 11:26:57 -0600

    Get the whole website in here

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const ejs = require('ejs');
 
-let data = JSON.parse(fs.readFileSync('resume_data.json', 'utf8'));
+let data = JSON.parse(fs.readFileSync('src/resume_data.json', 'utf8'));
 data.dateUpdated = new Date().toISOString().replace(/T.*/, '');
-const template_string = fs.readFileSync('template.ejs', 'utf8');
-fs.writeFileSync('website/resume.html', ejs.render(template_string, data));
+const template_string = fs.readFileSync('src/template.ejs', 'utf8');
+fs.writeFileSync('dist/resume/index.html', ejs.render(template_string, data));
