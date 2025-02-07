56ba7b0a874539301bc3ef2c2de2c89455eecb73
Stav Gold <ragaxus@gmail.com>
2025-02-06 20:15:14 -0600

    Beef up resume, use CST timezone for last updated

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -2,6 +2,6 @@ const fs = require('node:fs');
 const ejs = require('ejs');
 
 let data = JSON.parse(fs.readFileSync('src/resume_data.json', 'utf8'));
-data.dateUpdated = new Date().toISOString().replace(/T.*/, '');
+data.dateUpdated = new Date().toLocaleDateString('en-US', { timeZone: 'America/Chicago' });
 const template_string = fs.readFileSync('src/template.ejs', 'utf8');
 fs.writeFileSync('dist/resume/index.html', ejs.render(template_string, data));
